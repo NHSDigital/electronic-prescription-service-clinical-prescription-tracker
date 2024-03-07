53ed2d746ecf65f196e542ced815c30386d4ad78
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2024-03-07 15:31:09 +0000

    fix delete stacks (#79)
    ## Summary
    
    - Routine Change
    
    ### Details
    
    - fixes delete stacks to get correct pull request id

--- .github/scripts/delete_stacks.sh
+++ .github/scripts/delete_stacks.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-ACTIVE_STACKS=$(aws cloudformation list-stacks | jq -r '.StackSummaries[] | select ( .StackStatus != "DELETE_COMPLETE" ) | select( .StackName | capture("^clinical-tracker-pr-(sandbox-)?(\\d+)$") ) | .StackName ')
+ACTIVE_STACKS=$(aws cloudformation list-stacks | jq -r '.StackSummaries[] | select ( .StackStatus != "DELETE_COMPLETE" ) | select( .StackName | capture("^clinical-tracker-(sandbox-)?pr-(\\d+)$") ) | .StackName ')
 
 mapfile -t ACTIVE_STACKS_ARRAY <<< "$ACTIVE_STACKS"
 
 for i in "${ACTIVE_STACKS_ARRAY[@]}"
 do 
   echo "Checking if stack $i has open pull request"
-  PULL_REQUEST=${i//pr-/}
-  PULL_REQUEST=${PULL_REQUEST//sandbox-/}
+  PULL_REQUEST=${i//clinical-tracker-pr-/}
+  PULL_REQUEST=${PULL_REQUEST//clinical-tracker-sandbox-pr-/}
   echo "Checking pull request id ${PULL_REQUEST}"
   URL="https://api.github.com/repos/NHSDigital/electronic-prescription-service-clinical-prescription-tracker/pulls/${PULL_REQUEST}"
+
   RESPONSE=$(curl "${URL}" 2>/dev/null)
   STATE=$(echo "${RESPONSE}" | jq -r .state)
   if [ "$STATE" == "closed" ]; then
