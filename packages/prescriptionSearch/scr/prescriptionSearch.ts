f288e92e6145358e326e796f5e7a65d62a7cd559
Kris Szlapa <kris.szlapa1@nhs.net>
2024-09-18 08:15:57 +0000

    Add prescriptionSearch lambda

---
+++ packages/prescriptionSearch/scr/prescriptionSearch.ts
@@ -0,0 +1,52 @@
+import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda"
+import {Logger} from "@aws-lambda-powertools/logger"
+import {injectLambdaContext} from "@aws-lambda-powertools/logger/middleware"
+import {LogLevel} from "@aws-lambda-powertools/logger/types"
+import middy from "@middy/core"
+import inputOutputLogger from "@middy/input-output-logger"
+import errorHandler from "@nhs/fhir-middy-error-handler"
+import successData from "../examples/GetMyPrescriptions/Bundle/success.json"
+
+const LOG_LEVEL = process.env.LOG_LEVEL as LogLevel
+export const logger = new Logger({serviceName: "prescriptionSearch", logLevel: LOG_LEVEL})
+
+/* eslint-disable  max-len */
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  logger.appendKeys({
+    "nhsd-correlation-id": event.headers["nhsd-correlation-id"],
+    "x-request-id": event.headers["x-request-id"],
+    "nhsd-request-id": event.headers["nhsd-request-id"],
+    "x-correlation-id": event.headers["x-correlation-id"],
+    "apigw-request-id": event.requestContext.requestId
+  })
+  return {
+    statusCode: 200,
+    body: JSON.stringify(successData),
+    headers: {
+      "Content-Type": "application/fhir+json",
+      "Cache-Control": "no-cache"
+    }
+  }
+}
+
+export const handler = middy(lambdaHandler)
+  .use(injectLambdaContext(logger, {clearState: true}))
+  .use(
+    inputOutputLogger({
+      logger: (request) => {
+        logger.info(request)
+      }
+    })
+  )
+  .use(errorHandler({logger: logger}))
