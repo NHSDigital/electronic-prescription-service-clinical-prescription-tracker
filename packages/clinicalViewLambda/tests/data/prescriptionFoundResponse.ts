09212ac6d2b40d2cac7d1f806c829466c4d8679e
Jack Spagnoli <jack.spagnoli1@nhs.net>
2024-09-23 09:48:31 +0000

    eslint ignores test response

--- packages/clinicalViewLambda/tests/data/prescriptionFoundResponse.ts
+++ packages/clinicalViewLambda/tests/data/prescriptionFoundResponse.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 export default `<?xml version='1.0' encoding='UTF-8'?>
 <SOAP:Envelope xmlns:crs="http://national.carerecords.nhs.uk/schema/crs/"
     xmlns:SOAP="http://schemas.xmlsoap.org/soap/envelope/"
