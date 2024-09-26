7b432b30a11170066ecc643135db4974709f4b27
Kris Szlapa <kris.szlapa1@nhs.net>
2024-09-26 10:43:55 +0000

    Update nhsdigital package and imports

--- packages/prescriptionSearch/src/prescriptionSearch.ts
+++ packages/prescriptionSearch/src/prescriptionSearch.ts
@@ -1,10 +1,10 @@
-import {SpineClient} from "@NHSDigital/eps-spine-client/lib/spine-client"
+import {SpineClient} from "@nhsdigital/eps-spine-client/lib/spine-client"
 import {LogLevel} from "@aws-lambda-powertools/logger/types"
 import {Logger} from "@aws-lambda-powertools/logger"
-import {createSpineClient} from "@NHSDigital/eps-spine-client"
+import {createSpineClient} from "@nhsdigital/eps-spine-client"
 import {APIGatewayEvent, APIGatewayProxyResult} from "aws-lambda"
 import middy from "@middy/core"
-import {PrescriptionSearchParams} from "@NHSDigital/eps-spine-client/lib/live-spine-client"
+import {PrescriptionSearchParams} from "@nhsdigital/eps-spine-client/lib/live-spine-client"
 
 export const LOG_LEVEL = process.env.LOG_LEVEL as LogLevel
 export const logger = new Logger({serviceName: "prescriptionSearch", logLevel: LOG_LEVEL})
