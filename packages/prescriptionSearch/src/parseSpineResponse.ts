b1c3df98d21cc1a370c381e1d594f930a1f2492a
Adam Brown <adam.brown41@nhs.net>
2025-02-06 16:49:24 +0000

    [AEA-4929] added initial xml parsing

--- packages/prescriptionSearch/src/parseSpineResponse.ts
+++ packages/prescriptionSearch/src/parseSpineResponse.ts
@@ -1,43 +1,112 @@
 import {XMLParser} from "fast-xml-parser"
 
-import {erdExample} from "./examples/examples"
-
-// TODO: handle errors
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-type PrescriptionSearchResponse = {
-  patientDetails: {
-    nhsNumber: string
-    prefix: string
-    suffix: string
-    given: string
-    family: string
-  }
-  prescriptionDetails: {
-    prescriptionId: string
-    issueDate: string
-    treatmentType: string
-    maxRepeats: number
-  }
-  issues: Array<{
-    issueNumber: number
-    status: string
-    itemsPendingCancellation: boolean
-    itemsPendingCancellationCount: number
-    prescriptionPendingCancellation: boolean
-  }>
+interface PatientDetails {
+  nhsNumber: string
+  prefix: string
+  suffix: string
+  given: string
+  family: string
 }
 
-export const parseSpineResponse = (spineResponse: string) => {
+type PrescriptionDetails = {
+  prescriptionId: string
+  issueDate: string
+  treatmentType: string
+  maxRepeats: number | null
+}
+
+type IssueDetails = {
+  issueNumber: number
+  status: string
+  itemsPendingCancellation: boolean
+  itemsPendingCancellationCount: number
+  prescriptionPendingCancellation: boolean
+}
+type Prescription = {
+  patientDetails: PatientDetails
+  prescriptionDetails: PrescriptionDetails
+  issues: Array<IssueDetails>
+}
+
+type PrescriptionSearchResult = Array<Prescription>
+
+export const parseSpineResponseXml = (spineResponseXml: string) => {
+  let prescriptionSearchResult: PrescriptionSearchResult = []
+
   const parserOptions = {
     ignoreAttributes: false
   }
   const xmlParser = new XMLParser(parserOptions)
+  let parsedResponseXml = xmlParser.parse(spineResponseXml)
 
-  let jObj = xmlParser.parse(spineResponse)
-  return jObj["SOAP:Envelope"]["SOAP:Body"].prescriptionSearchResponse.PRESCRIPTIONSEARCHRESPONSE_SM01.id
+  const soapBody = parsedResponseXml["SOAP:Envelope"]?.["SOAP:Body"]
+  if (!soapBody) {
+    // TODO: error stuff
+    console.log("error")
+    return
+  }
+
+  // eslint-disable-next-line max-len
+  const searchResults = soapBody.prescriptionSearchResponse.PRESCRIPTIONSEARCHRESPONSE_SM01.ControlActEvent.subject.searchResults
+  let prescriptions = searchResults.prescription
+  if (!Array.isArray(prescriptions)) {
+    prescriptions = [prescriptions]
+  }
+  return prescriptions
 }
 
-export const parseErd = () => {
-  return parseSpineResponse(erdExample)
+type XmlStringValue = {
+  "@_value": string
+}
+
+interface IssueDetail {
+  instanceNumber: XmlStringValue
+  prescriptionStatus: XmlStringValue
+  cancellations: XmlStringValue
+  prescCancPending: XmlStringValue
+  liCancPending: XmlStringValue
+}
+
+interface ResponsePrescription {
+  id: XmlStringValue
+  patientId: XmlStringValue
+  prefix: XmlStringValue
+  suffix: XmlStringValue
+  given: XmlStringValue
+  family: XmlStringValue
+  issueDetail: IssueDetail | Array<IssueDetail>
+  prescribedDate: XmlStringValue
+  prescriptionTreatmentType: XmlStringValue
+  maxRepeats: XmlStringValue
+}
+
+const parsePrescriptions = (prescriptions: Array<ResponsePrescription>) => {
+  for (const prescription of prescriptions) {
+    // do stuff
+    const patientDetails: PatientDetails = {
+      nhsNumber: prescription.patientId["@_value"],
+      prefix: prescription.prefix["@_value"],
+      suffix: prescription.suffix["@_value"],
+      given: prescription.given["@_value"],
+      family: prescription.family["@_value"]
+    }
+
+    const prescriptionDetails: PrescriptionDetails = {
+      prescriptionId: prescription.id["@_value"],
+      issueDate: prescription.prescribedDate["@_value"],
+      treatmentType: prescription.prescriptionTreatmentType["@_value"],
+      maxRepeats: prescription.maxRepeats["@_value"] === "None" ? null : Number(prescription.maxRepeats["@_value"])
+    }
+    let issues = prescription.issueDetail
+    if (!Array.isArray(issues)) {
+      issues = [issues]
+    }
+    for (const issue of issues) {
+      const issueDetails: IssueDetail = {
+        instanceNumber: issue.instanceNumber["@_value	"],
+        status: issue.prescriptionStatus
+
+      }
+    }
+  }
 }
--- packages/prescriptionSearch/tests/test-parse.test.ts
+++ packages/prescriptionSearch/tests/test-parse.test.ts
@@ -1,8 +1,19 @@
-import {parseErd} from "../src/parseSpineResponse"
+import {parseSpineResponseXml} from "../src/parseSpineResponse"
+import {erdExample, multipleExample, errorExample} from "../src/examples/examples"
 
-describe("Test Parse", () => {
+describe("Test ERD", () => {
   it("does a parse", async () => {
-    const result = parseErd()
+    const result = parseSpineResponseXml(erdExample)
+    console.log(result)
+  })
+
+  it("Test Multiple", async () => {
+    const result = parseSpineResponseXml(multipleExample)
+    console.log(result)
+  })
+
+  it("an error", async () => {
+    const result = parseSpineResponseXml(errorExample)
     console.log(result)
   })
 })
