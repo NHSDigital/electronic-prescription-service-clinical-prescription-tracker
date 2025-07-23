52a6e6a872bc70efb3430a8206cbe714a195e2ca
Adam Brown <adam.brown41@nhs.net>
2025-07-23 16:30:05 +0000

    [AEA-5565] adds prescription id validation

--- packages/common/commonUtils/src/commonValidation.ts
+++ packages/common/commonUtils/src/commonValidation.ts
@@ -68,3 +68,62 @@ export const validateCommonHeaders = (
 
   return [headerParameters, errors]
 }
+
+const PRESCRIPTION_ID_PATTERN = /^[0-9a-fA-F]{6}-[0-9a-fA-F]{6}-[0-9a-fA-F]{5}[0-9a-zA-Z+]{1}$/
+const PRESCRIPTION_ID_WITHOUT_CHECKSUM_LENGTH = 17 as const
+const CHECKSUM_CHARACTERS = [..."0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ+"] as const
+
+export const validatePrescriptionId = (prescriptionId: string, logger: Logger): Array<ServiceError> => {
+  const errors: Array<ServiceError> = []
+
+  const validFormat = PRESCRIPTION_ID_PATTERN.test(prescriptionId)
+  if (!validFormat) {
+    logger.error("prescriptionId does not match required format.", {prescriptionId})
+    errors.push({
+      status: 400,
+      severity: "error",
+      description: "prescriptionId does not match required format."
+    })
+  }
+
+  const idWithoutDelimiters = prescriptionId.replaceAll("-", "")
+  const [idWithoutChecksum, checksum] = idWithoutDelimiters.split(/(?<=^.{17})/)
+
+  let total = 0
+  for (const [charIndex, char] of [...idWithoutChecksum].entries()){
+    const charMod36 = parseInt(char, 36)
+    total += charMod36 * (2 ** (PRESCRIPTION_ID_WITHOUT_CHECKSUM_LENGTH - charIndex))
+  }
+
+  const calculatedChecksumValue = (38 - total % 37) % 37
+  const calculatedChecksumChar = CHECKSUM_CHARACTERS[calculatedChecksumValue]
+
+  if (calculatedChecksumChar !== checksum){
+    logger.error("Invalid prescriptionId checksum.", {prescriptionId, checksum, calculatedChecksumChar})
+    errors.push({
+      status: 400,
+      severity: "error",
+      description: "prescriptionId checksum is invalid."
+    })
+  }
+
+  return errors
+}
+
+const NHS_NUMBER_PATTERN = /^\d{10}$/
+
+export const validateNhsNumber = (nhsNumber: string, logger: Logger): Array<ServiceError> => {
+  const errors: Array<ServiceError> = []
+
+  const validFormat = NHS_NUMBER_PATTERN.test(nhsNumber)
+  if (!validFormat) {
+    logger.error("nhsNumber does not match required format.", {nhsNumber})
+    errors.push({
+      status: 400,
+      severity: "error",
+      description: "nhsNumber does not match required format."
+    })
+  }
+
+  return errors
+}
