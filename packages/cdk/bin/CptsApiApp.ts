d4d03368665cb0ea834fe8ea09dd6f143c058d94
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2025-08-01 07:39:01 +0000

    set dummy value

--- packages/cdk/bin/CptsApiApp.ts
+++ packages/cdk/bin/CptsApiApp.ts
@@ -23,7 +23,6 @@ const cfnDriftDetectionGroup = app.node.tryGetContext("cfnDriftDetectionGroup")
 
 Aspects.of(app).add(new AwsSolutionsChecks({verbose: true}))
 
-Tags.of(app).add("accountId", accountId)
 Tags.of(app).add("stackName", stackName)
 Tags.of(app).add("version", version)
 Tags.of(app).add("commit", commit)
